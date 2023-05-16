b21a30f34ee060a799cc9ae112d752824745c84d
GabrielFelipe1507 <gabriel_felipe_trabalho@hotmail.com>
2023-05-16 11:20:56 -0300

    atualizacao1.3

--- calculadora/calculadora.c
+++ calculadora/calculadora.c
@@ -1,27 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-
-    float num1, num2, mult, soma, sub, div;
-
-    printf("digite o primeiro numero:");
-    scanf("%f", &num1);
-
-    printf("digite o segundo numero:");
-    scanf("%f", &num2);
-
-    mult = num1*num2;
-    soma = num1 + num2;
-    div = num1 / num2;
-    sub = num1 - num2;
-
-    printf("\nmultiplicacao: %.2f", mult);
-
-    printf("\nsoma: %.2f", soma);
-
-    printf("\ndivisao: %.2f", div);
+void subtracao(float num1, float num2)
+{
+    float sub = num1 - num2;
 
     printf("\nsubtracao: %.2f\n", sub);
+}
 
+void divisao(float num1, float num2)
+{
+    float div = num1 / num2;
+    printf("\ndivisao: %.2f", div);
+}
+
+void multiplicacao(float num1, float num2)
+{
+    float mult = num1 * num2;
+
+    printf("\nmultiplicacao: %.2f", mult);
+}
+
+void soma(float num1, float num2)
+{
+    float soma = num1 + num2;
+
+    printf("\nsoma: %.2f", soma);
+}
+
+void main()
+{
+
+    float num1, num2;
+
+    printf("igite o primeiro numero:");
+    scanf("%f", &num1);
+
+    printf("Digite o segundo numero:");
+    scanf("%f", &num2);
+
+    //chamando as funções da calculadora:
+    soma(num1, num2);
+
+    multiplicacao(num1, num2);
+
+    divisao(num1, num2);
+
+    subtracao(num1, num2);
 }
